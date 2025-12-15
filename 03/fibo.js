97419f7a9a15e7239e15cc1f0aaf0541dbab4ad1
Sanya <kartuzovaleksandr@yandex.ru>
2025-12-15 16:22:03 +0300

    Перепланировка уроков, много новых примеров

---
+++ 03/fibo.js
@@ -0,0 +1,45 @@
+function fibo(number) {
+  let fib= [];
+  for (let i = 0; i < number; i++) {
+    fib[i] = fibonacchi(i);
+  }
+  return fib;
+}
+
+function fibonacchi(x) {
+    if (x === 0 || x === 1 || x === 2)
+        return 1
+    else
+        return fibonacchi(x - 1) + fibonacchi(x - 2);
+}
+
+function checkValidData(number) {
+  if (number === null || number.trim() === "") {
+    alert("Поле не может быть пустым.");
+    return false;
+  }
+  const num = Number(number);
+  if (isNaN(num)) {
+    alert("Введите целое число");
+    return false;
+  }
+
+  if (!Number.isInteger(num)) {
+    alert("Введите целое число");
+    return false;
+  }
+
+  if (num < 2 || num > 20) {
+    alert("Введите число больше 2 и меньше 20");
+    return false;
+  }
+  return true;
+}
+
+const userInput = prompt("Введите число:");
+if (checkValidData(userInput)) {
+  const number = parseInt(userInput);
+  const mass = fibo(number);
+  document.write(`Первые ${number} чисел Фибоначчи: ${mass.join(", ")}`);
+  document.write(`<br>Среднее арифметическое: ${mass.reduce((a, b) => a + b) / mass.length}`);
+}
